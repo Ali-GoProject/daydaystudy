ae56dcbcc959c6e03ff33261df2795bc03f68fa7
x <x@HUITAOdeMacBook-Pro.local>
2019-08-29 08:59:49 +0800

    提交第一个demo，hello world诞生

---
+++ demo/hello-world.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt"
+		"os")
+func main() {
+	fmt.Println("hello,world")
+	fmt.Println(os.Getenv("HOME"))
+	fmt.Println(os.Getenv("GOROOT"))
+	fmt.j
+	fmt.Println(os.Getenv("GOROOT"))
+}
